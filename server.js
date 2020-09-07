0112b3b9bf55a4e3781e2dd286d1bbf5a01cba25
unknown <jean-pierre14>
2020-09-07 17:41:33 +0200

    API into indes.html

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const exp = require('express')
-const fs = require('fs')
-const url = require('url')
-const path = require('path')
+const cors = require('cors')
 const app = exp()
 const PORT = process.env.PORT || 7000
 const routers = require('./routes/posts')
 const db = require('./models/')
+
+app.use(cors())
 
 app.get('/', (req, res)=>{
     res.send('Welcome to my APIs go to this link localhost:7000/Api and enjoy that')
