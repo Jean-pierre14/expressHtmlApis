4b2f6fd723189976ed7e0e92038d1d86d7420cc9
Jean-pierre14 <chiruzabisimwa@gmail.com>
2022-04-13 22:36:47 -0700

    Notification Event

--- javascript.js
+++ javascript.js
@@ -2,21 +2,57 @@ document.getElementById('img').addEventListener('change', ShowImg);
 
 function ShowImg(evt) {
 
-    var f = evt.target.files;
+    var files = evt.target.files,
+        f = files[0];
+
+    console.log(`This is the file name: ${f.name}`)
+    console.log(`This is the file size: ${f.size}k`)
+
+    // ou can also get easily the following attributes: lastModified (Timestamp), lastModifiedDate (Date), and type (FileType)
+
+    console.log(`Last modification :: ${f.lastModified}`)
+    console.log(`Last modification Date :: ${f.lastModifiedDate}`)
 
     // alert("Changed") Test the event
 
-    if (f.length === 0) {
+    if (files.length === 0) {
         console.log('No file uploaded...');
         return;
     }
 
     var reader = new FileReader();
     reader.onload = function(event) {
-        let img = new Image();
+        var img = new Image();
         img.onload = function() {
-            document.body.appendChild(img);
+            document.getElementById('result').appendChild(img);
         };
-        reader.readAsDataURL(files[0]);
+        img.src = event.target.result;
     };
+    reader.readAsDataURL(files[0]);
 }
+
+
+function sliceFile(file, chunksAmount) {
+    var byteIndex = 0,
+        chunks = [];
+
+    for (var n = 1; n < chunksAmount; n++) {
+        var byteEnd = Math.ceil((file.size / chunksAmount) * (i + 1));
+        chunks.push(file.slice(byteIndex, byteEnd));
+
+        byteIndex += (byteEnd - byteIndex)
+    }
+    return file;
+}
+
+// Notification permission
+
+Notification.requestPermission(() => {
+    if (Notification.permission === 'granted') {
+        alert("Cool")
+    } else if (Notification.permission === 'denied') {
+        alert('Notifications Denied')
+    } else {
+        alert('Default')
+    }
+})
