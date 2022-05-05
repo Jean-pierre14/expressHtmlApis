01e563b3b85128b17a19381254ad272c7032f353
Jean-pierre14 <chiruzabisimwa@gmail.com>
2022-05-04 22:04:57 -0700

    Fetch data and get them into HTML using foreach loop

--- fetch.js
+++ fetch.js
@@ -10,10 +10,19 @@ const SelectAll = () => {
     fetch('http://localhost:7000/api')
         .then(res => res.json())
         .then(data => {
-            if (data.length > 0) {
+            if (data.datas.length > 0) {
 
+                data.datas.forEach(element => {
+                    results.innerHTML += `
+                    <div id="${element.id}" class="card shadow-sm my-2">
+                        <div class="card-body">
+                            <p>${element.username}</p>
+                            <p>${element.email}</p>
+                        </div>
+                    </div>`
+                });
             } else {
-
+                results.innerHTML = '<p class="alert alert-danger">There is no data</p>'
             }
         })
 }
